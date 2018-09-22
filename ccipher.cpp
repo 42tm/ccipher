f20906e7b81fec983d54bb8089e9b9846857df2a
Minh Duc Vo <minhducsun2002@gmail.com>
2018-09-22 23:49:56 +0700

    Add proper ccipher binary implementation. Version 0.1.0-alpha.

--- ccipher.cpp
+++ ccipher.cpp
@@ -1,15 +1,68 @@
 // require C++11 or newer version
-
 #include "lib/atbash.hpp"
 #include "lib/caesar.hpp"
 #include "lib/vigenere.hpp"
 
 #include <iostream>
 #include <string>
+#include <unordered_set>
+#include <stdexcept>
 using std::cout;
+
+std::unordered_set <std::string> ciphers;
+
+void printVersionInformation();
+void printHelpInformation();
+void initializeResources();
+
+int main(int argc, char *argv[])
+{
+    initializeResources();
+    if (argc == 1 || argv[1] == std::string("-h") || argv[1] == std::string("--help"))
+    {
+        cout << "ccipher - Encrypt text strings using classical ciphers.\n";
+        printVersionInformation();
+        printHelpInformation();
+        return 0;
+    }
+    std::string cipher (argv[1]);
+    std::string command (argv[2]);
+    std::string plaintext (argv[3]);
+    if (cipher == "atbash")
+        return cout << atbash(plaintext), 0;
+    if (cipher == "rot13")
+        return cout << rot13(plaintext), 0;
+    if (cipher == "caesar")
+    {
+        int shift;
+        try 
+        {
+            shift = std::stoi(std::string(argv[4])) % 26;
+            if (command == "d") shift = 26 - shift;
+        }
+        catch (std::exception e)
+        {
+            return std::cerr << "Caesar cipher : Invalid shift value encountered.\n", 1;
+        }
+        return cout << caesar(plaintext, shift), 0;
+    }
+    if (cipher == "vigenere")
+    {
+        try 
+        {
+            if (command == "d") return cout << vigenere::decrypt(plaintext, std::string(argv[4])), 0;
+            else return cout << vigenere::encrypt(plaintext, std::string(argv[4])), 0;
+        }
+        catch (std::exception &e)
+        {
+            return std::cerr << "Vigènere : key contains non-alphabetic character" << std::endl, 1;
+        }
+    }
+}
 
 void printVersionInformation()
 {
+    using std::cout;
     // versioning
     cout << "Version : ";
     #ifdef _CCIPHER_VERSION
@@ -25,10 +78,12 @@ void printVersionInformation()
     #elif defined(__GNUC__)
         std::cout
             << "GNU C++ Compiler " << __GNUC__ << "." << __GNUC_MINOR__ << "." << __GNUC_PATCHLEVEL__;
-        #ifdef defined(__MINGW64__)
+        #if defined(__MINGW64__)
             std::cout << " (MinGW-w64 64-bit)\n";
         #elif defined(__MINGW32__)
             std::cout << " (MinGW/MinGW-w64 32-bit)\n";
+        #else
+            std::cout << '\n';
         #endif
     #elif defined(_MSC_VER)
         std::cout
@@ -39,23 +94,26 @@ void printVersionInformation()
 
     // compilation time
     #ifdef __GNUC__
-        cout << "Compiled on " << __DATE__ << ", " << __TIME__ << endl;
+        cout << "Compiled on " << __DATE__ << ", " << __TIME__ << std::endl;
     #endif
 }
 
 void printHelpInformation()
 {
-    
+    using std::cout;
+    std::cout   << "ccipher help : ccipher <cipher> <e|d> [args]\n";
+    std::cout   << "\te means encrypt. d means decrypt.\n";
+    std::cout   << "Possible values for <cipher> :\n"
+                << "\tatbash - Atbash cipher. Arguments passed : [text]\n"
+                << "\trot13 - ROT13 cipher. Arguments passed : [text]\n"
+                << "\tcaesar - Caesar cipher. Arguments passed : [text] [shift]\n"
+                << "\tvigenere - Vigènere cipher. Arguments passed : [text] [key]\n";
 }
 
-int main(int argc, char *argv[])
+void initializeResources()
 {
-    if (argc == 1 || argv[1] == std::string("-h") || argv[1] == std::string("--help"))
-    {
-        cout << "ccipher - Encrypt text strings using classical ciphers.\n";
-        printVersionInformation();
-        printHelpInformation();
-        return 0;
-    }
-
+    ciphers.insert("atbash");
+    ciphers.insert("vigenere");
+    ciphers.insert("caesar");
+    ciphers.insert("rot13");
 }
--- lib/vigenere.hpp
+++ lib/vigenere.hpp
@@ -1,6 +1,19 @@
-/* 
- * vigenere - Library for the vigenere cipher
+/*
+ * vigenere - Library for the vigenere cypher
  * Copyright (C) 2018 42tm Team
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #pragma once
@@ -14,10 +27,18 @@ class vigenere
     static std::string encrypt (std::string plaintext, std::string key);
     static std::string decrypt (std::string plaintext, std::string key);
     private:
+    static void verify(std::string key);
     static int CharToInt (const char ch);
     static char IntToChar(const int i, bool isUpper = true);
     static char combine(char ch1, char ch2);
     static char divide(char ch1, char ch2);
+};
+
+void vigenere::verify(std::string key)
+{
+    for (const auto I : key)
+        if (!isalpha(I))
+            throw std::invalid_argument("Vigenere : key contains non-alphabetic character");
 }
 
 int vigenere::CharToInt(const char ch)
@@ -49,6 +70,14 @@ char vigenere::divide(char ch1, char ch2)
 
 std::string vigenere::encrypt(std::string input, std::string key)
 {
+    try 
+    {
+        verify(key);
+    }
+    catch (std::exception &e)
+    {
+        throw e;
+    }
     long k = 0;
     for (auto &itr : input)
         if (isalpha(itr))
@@ -58,6 +87,14 @@ std::string vigenere::encrypt(std::string input, std::string key)
 
 std::string vigenere::decrypt(std::string input, std::string key)
 {
+    try 
+    {
+        verify(key);
+    }
+    catch (std::exception &e)
+    {
+        throw e;
+    }
     long k = 0;
     for (auto &itr : input)
         if (isalpha(itr))
